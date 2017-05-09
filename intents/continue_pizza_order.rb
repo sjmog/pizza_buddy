29c4ddd8ae11a6b23a065ed3d0a571bfc924d347
Sam Morgan <s_morgan@me.com>
2017-05-09 17:24:03 +0100

    Add ContinuePizzaOrder intent
    - save the user's requested size to the session
    - prompt the user to give which items they want on the pizza
    - offer a list of options instead

---
+++ intents/continue_pizza_order.rb
@@ -0,0 +1,10 @@
+require './lib/pizza'
+
+intent "ContinuePizzaOrder" do
+  size = request.slot_value("size")
+  response_text = ["OK, a #{ size } pizza. What would you like on that pizza? ",
+                   "You can choose up to five items, or ask for a list of ",
+                   "choices. Or, choose another size: #{ Pizza::SIZES.to_sentence }"].join
+  
+  ask(response_text, session_attributes: { size: size })
+end
