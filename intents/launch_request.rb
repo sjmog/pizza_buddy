93559d941c69d1a734178aaaf15682b19fcd007d
Sam Morgan <s_morgan@me.com>
2017-05-30 19:46:04 +0100

    Add a catch on launch unless the user has authenticated via OAuth (i.e. an Access Token exists)
    - OAuth setup is here: https://developer.amazon.com/blogs/post/Tx3CX1ETRZZ2NPC/Alexa-Account-Linking-5-Steps-to-Seamlessly-Link-Your-Alexa-Skill-with-Login-wit

--- intents/launch_request.rb
+++ intents/launch_request.rb
@@ -1,3 +1,7 @@
 intent "LaunchRequest" do
-  ask("Welcome to Pizza Buddy. Would you like a new pizza, or to list orders?")
+  if request.user_access_token_exists?
+    ask("Welcome to Pizza Buddy. Would you like a new pizza, or to list orders?")
+  else
+    tell("Please authenticate Pizza Buddy via the Alexa app.", card: link_account_card)
+  end
 end
