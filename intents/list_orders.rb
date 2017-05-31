13d5f9646cf9f7f99cbb1fc06962be528c689a13
Sam Morgan <s_morgan@me.com>
2017-05-31 13:30:48 +0100

    Users can only list orders they made
    - Hide ListOrders behind an authentication wall
    - List only user.pizzas instead of Pizza.all

--- intents/list_orders.rb
+++ intents/list_orders.rb
@@ -1,11 +1,17 @@
 require './lib/pizza'
 
 intent "ListOrders" do
-  orders = Pizza.first(4).map { |order| "a #{ order.size } pizza with #{ order.toppings.to_sentence }" }
+  return tell("To list your orders, please authenticate Pizza Buddy via the Alexa app.", card: link_account_card) unless request.user_access_token_exists?
 
-  response_text = ["There are #{ Pizza.count } orders. ",
-                   "#{ orders.to_sentence }.",
-                   "You can ask to list orders again, or order a pizza."].join
+  user = ::User.authenticate(request.user_access_token)
+  if user.pizzas.any?
+    orders = user.pizzas.first(4).map { |order| "a #{ order.size } pizza with #{ order.toppings.to_sentence }" }
+    response_text = ["You have made #{ user.pizzas.count } orders. ",
+                     "#{ orders.to_sentence }. ",
+                     "You can ask to list orders again, or order a pizza."].join
+  else
+    response_text = "You haven't made any orders yet. To start, say 'order a pizza'."
+  end
 
   ask(response_text)
 end
